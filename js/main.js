297192e9f05a121abd2c021e832143cb81a651c6
iboates <iboates@gmail.com>
2019-02-17 13:07:25 +0100

    forgot i even initialized this, it is working now, also added a sidebar and control over spotlight shape

--- js/main.js
+++ js/main.js
@@ -4,22 +4,104 @@
 
 $(document).ready(function() {
 
-    var pointStyle = {
-        radius: 8,
-        fillColor: "#4444ff",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.5
+    // Set the initial spotlight shape to a circle and bind an event listener to it to change shape
+    var spotlightShape = $("#shape-selector").val();
+    $("#shape-selector").on("change", function() {
+        spotlightShape = $("#shape-selector").val();
+    });
+
+    var styleData = {
+        "min0": null,
+        "max0": null,
+        "min1": null,
+        "max1": null,
+        "min2": null,
+        "max2": null,
+        "min3": null
     };
 
-    var spotlightStyle = {
-        radius: 10,
-        fillColor: "#ffff00",
-        color: "#000",
+    function getExtreme(gj, prop, minOrMax) {
+
+        var featureId = -1;
+
+        if (minOrMax == "max") {
+            var max = -999999;
+            for (var i=0; i<gj.features.length; i++) {
+                if (gj.features[i].properties[prop] > max) {
+                    max = gj.features[i].properties[prop];
+                    featureId = gj.features[i].properties.id;
+                }
+            }
+        } else if (minOrMax == "min") {
+            var min = 999999;
+            for (var i=0; i<gj.features.length; i++) {
+                if (gj.features[i].properties[prop] < min) {
+                    min = gj.features[i].properties[prop];
+                    featureId = gj.features[i].properties.id;
+                }
+            }
+        }
+
+        return featureId
+
+    };
+
+    var pointStyle = {
+        radius: 2,
+        fillColor: "#000000",
+        color: "#000000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 1
+        fillOpacity: 0
+    };
+
+    function spotlightStyle2(feature) {
+        return feature.properties.id == styleData.max0 ? {
+            radius: 8,
+            fillColor: "#880000",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
+        } : feature.properties.id == styleData.max1 ? {
+            radius: 8,
+            fillColor: "#008800",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
+        } : feature.properties.id == styleData.max2 ? {
+            radius: 8,
+            fillColor: "#000088",
+            color: "#000000",
+            weight: 1,
+            opacity: 0,
+            fillOpacity: 1
+        } : feature.properties.id == styleData.min0 ? {
+            radius: 8,
+            fillColor: "#ffaaaa",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
+        } : feature.properties.id == styleData.min1 ? {
+            radius: 8,
+            fillColor: "#aaffaa",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
+        } : feature.properties.id == styleData.min2 ? {
+            radius: 8,
+            fillColor: "#aaaaff",
+            color: "#000000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1
+        } : {
+            opacity: 0,
+            fillOpacity: 0
+        }
     };
 
     // Initialize map & layers
@@ -31,20 +113,48 @@ $(document).ready(function() {
         }
      }).addTo(map);
 
+    var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+    var spotlightHighlightLayer;
     var spotlightLayer;
     var mousePosition = L.latLng(0, 0);
-    var mouseCircle;
+    var mouseShape;
     map.addEventListener('mousemove', function(ev) {
+
+        if (map.hasLayer(spotlightHighlightLayer)) {
+            map.removeLayer(spotlightHighlightLayer);
+        }
 
         if (map.hasLayer(spotlightLayer)) {
             map.removeLayer(spotlightLayer)
         }
 
-        mouseCircle = turf.circle([ev.latlng.lng, ev.latlng.lat], 0.05, {"steps": 16, "units": "kilometers"});
-        var spotlightPoints = turf.pointsWithinPolygon(pointLayer.toGeoJSON(), mouseCircle);
-        spotlightLayer = L.geoJSON(spotlightPoints, {
+        if (spotlightShape == "circle") {
+            mouseShape = turf.circle([ev.latlng.lng, ev.latlng.lat], 50, {"steps": 128, "units": "meters"});
+        } else if (spotlightShape == "rectangle") {
+            mouseShape = turf.envelope(turf.featureCollection([
+                turf.circle([ev.latlng.lng, ev.latlng.lat], 50, {"steps": 128, "units": "meters"})
+            ]));
+        }
+        var spotlightPoints = turf.pointsWithinPolygon(pointLayer.toGeoJSON(), mouseShape);
+
+        styleData.max0 = getExtreme(spotlightPoints, "attr0", "max");
+        styleData.max1 = getExtreme(spotlightPoints, "attr1", "max");
+        styleData.max2 = getExtreme(spotlightPoints, "attr2", "max");
+        styleData.min0 = getExtreme(spotlightPoints, "attr0", "min");
+        styleData.min1 = getExtreme(spotlightPoints, "attr1", "min");
+        styleData.min2 = getExtreme(spotlightPoints, "attr2", "min");
+
+        spotlightHighlightLayer = L.geoJSON(spotlightPoints, {
             pointToLayer: function (feature, latlng) {
-                return L.circleMarker(latlng, spotlightStyle);
+                return L.circleMarker(latlng, spotlightStyle2(feature))
+            }
+        }).addTo(map);
+
+        spotlightLayer = L.geoJSON(mouseShape, {
+            style: {
+                color: "#000000",
+                fillOpacity: 0
             }
         }).addTo(map);
 
