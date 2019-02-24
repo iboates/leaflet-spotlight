d5b7c7b8cfc994a13b27dc70a9fa2357352b1bc3
iboates <iboates@gmail.com>
2019-02-24 17:15:17 +0100

    much more smartlier refactored, now just one event listener loops through all spotlights with their params saved in a registry

--- js/leaflet-spotlight.js
+++ js/leaflet-spotlight.js
@@ -6,6 +6,43 @@
         // As spotlights are added, a UUID will be generated for one which serves here as a key to its layers.
         this[0]._leafletSpotlight = {};
 
+        // Create a mousemove event listener for this spotlight
+        this[0].addEventListener('mousemove', function(ev) {
+
+            for (var uuid in this._leafletSpotlight) {
+            
+                var currentSpotlight = this._leafletSpotlight[uuid];
+
+                // On each mouse movement, remove the spotlight & highlighted features layer for this UUID
+                if (this.hasLayer(currentSpotlight._spotlightHighlightLayer)) {
+                    this.removeLayer(currentSpotlight._spotlightHighlightLayer);
+                }
+                if (this.hasLayer(currentSpotlight._spotlightLayer)) {
+                    this.removeLayer(currentSpotlight._spotlightLayer);
+                }
+
+                // Find which points are highlighted by seeing if they are within the spotlight
+                var highlightedPoints = turf.pointsWithinPolygon(
+                    pointLayer.toGeoJSON(),
+                    currentSpotlight._spotlightShape([ev.latlng.lng, ev.latlng.lat])
+                );
+
+                // Add the highlighted features to the map as a layer
+                currentSpotlight._spotlightHighlightLayer = L.geoJSON(highlightedPoints, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, currentSpotlight._highlightStyle(feature))
+                    }
+                }).addTo(this);
+
+                // Add the spotlight to the map as a layer
+                currentSpotlight._spotlightLayer = L.geoJSON(currentSpotlight._spotlightShape([ev.latlng.lng, ev.latlng.lat]), {
+                    style: currentSpotlight._spotlightStyle
+                }).addTo(this);
+
+            }
+
+        });
+
     }
 
     $.fn.addSpotlight = function(pointLayer, spotlightShape, spotlightStyle, highlightStyle) {
@@ -16,38 +53,12 @@
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
-            this[0]._leafletSpotlight[uuid] = {};
-
-            // Create a mousemove event listener for this spotlight
-            this[0].addEventListener('mousemove', function(ev) {
-
-                // On each mouse movement, remove the spotlight & highlighted features layer for this UUID
-                if (this.hasLayer(this._leafletSpotlight[uuid]._spotlightHighlightLayer)) {
-                    this.removeLayer(this._leafletSpotlight[uuid]._spotlightHighlightLayer);
-                }
-                if (this.hasLayer(this._leafletSpotlight[uuid]._spotlightLayer)) {
-                    this.removeLayer(this._leafletSpotlight[uuid]._spotlightLayer);
-                }
-
-                // Generate a geometry at the current mouse coordinates using the dynamic shape generator function
-                var mouseShape = spotlightShape([ev.latlng.lng, ev.latlng.lat]);
-
-                // Find which points are highlighted by seeing if they are within the spotlight
-                var highlightedPoints = turf.pointsWithinPolygon(pointLayer.toGeoJSON(), mouseShape);
-
-                // Add the highlighted features to the map as a layer
-                this._leafletSpotlight[uuid]._spotlightHighlightLayer = L.geoJSON(highlightedPoints, {
-                    pointToLayer: function (feature, latlng) {
-                        return L.circleMarker(latlng, highlightStyle(feature))
-                    }
-                }).addTo(this);
-
-                // Add the spotlight to the map as a layer
-                this._leafletSpotlight[uuid]._spotlightLayer = L.geoJSON(mouseShape, {
-                    style: spotlightStyle
-                }).addTo(this);
-
-            });
+            this[0]._leafletSpotlight[uuid] = {
+                "_targetLayer": pointLayer,
+                "_spotlightShape": spotlightShape,
+                "_spotlightStyle": spotlightStyle,
+                "_highlightStyle": highlightStyle
+            };
 
             // Returns this UUID so it can be used to remove the spotlight later if necessary.
             return uuid;
@@ -64,39 +75,7 @@
             this[0].removeLayer(this[0]._leafletSpotlight[spotlightUUID]._spotlightLayer);
         }
 
-        // Remove the event listener
-//        this[0].off("mousemove");
-
-        // Create a mousemove event listener for this spotlight
-        this[0].removeEventListener('mousemove', function(ev) {
-
-            // On each mouse movement, remove the spotlight & highlighted features layer for this spotlightUUID
-            if (this.hasLayer(this._leafletSpotlight[spotlightUUID]._spotlightHighlightLayer)) {
-                this.removeLayer(this._leafletSpotlight[spotlightUUID]._spotlightHighlightLayer);
-            }
-            if (this.hasLayer(this._leafletSpotlight[spotlightUUID]._spotlightLayer)) {
-                this.removeLayer(this._leafletSpotlight[spotlightUUID]._spotlightLayer);
-            }
-
-            // Generate a geometry at the current mouse coordinates using the dynamic shape generator function
-            var mouseShape = spotlightShape([ev.latlng.lng, ev.latlng.lat]);
-
-            // Find which points are highlighted by seeing if they are within the spotlight
-            var highlightedPoints = turf.pointsWithinPolygon(pointLayer.toGeoJSON(), mouseShape);
-
-            // Add the highlighted features to the map as a layer
-            this._leafletSpotlight[spotlightUUID]._spotlightHighlightLayer = L.geoJSON(highlightedPoints, {
-                pointToLayer: function (feature, latlng) {
-                    return L.circleMarker(latlng, highlightStyle(feature))
-                }
-            }).addTo(this);
-
-            // Add the spotlight to the map as a layer
-            this._leafletSpotlight[spotlightUUID]._spotlightLayer = L.geoJSON(mouseShape, {
-                style: spotlightStyle
-            }).addTo(this);
-
-        });
+        delete this[0]._leafletSpotlight[spotlightUUID];
 
     }
 
