3f081a42451817c29861c9ba2d9d31ac07faccc9
iboates <iboates@gmail.com>
2019-03-06 19:59:58 +0100

    added refresh logic to spotlightHandler

--- js/leaflet-spotlight-extension.js
+++ js/leaflet-spotlight-extension.js
@@ -1,3 +1,9 @@
+L.Map.include({
+
+    _spotlightRegistry: {},
+
+});
+
 L.SpotlightHandler = L.Handler.extend({
 
     addHooks: function() {
@@ -10,20 +16,53 @@ L.SpotlightHandler = L.Handler.extend({
 
     _doSomething: function(ev) {
 
-        console.log(this._map)
+        // Create a mousemove event listener for this spotlight
+        for (var spotlightId in this._map._spotlightRegistry) {
+        
+            var currentSpotlight = this._map._spotlightRegistry[spotlightId];
 
+            // On each mouse movement, remove the spotlight & highlighted features layer for this._map spotlightId
+            if (this._map.hasLayer(currentSpotlight.spotlightHighlightLayer)) {
+                this._map.removeLayer(currentSpotlight.spotlightHighlightLayer);
+            }
+            if (this._map.hasLayer(currentSpotlight.spotlightLayer)) {
+                this._map.removeLayer(currentSpotlight.spotlightLayer);
+            }
+
+            // Find which points are highlighted by seeing if they are within the spotlight
+            var highlightedPoints = turf.pointsWithinPolygon(
+                this._map._spotlightRegistry[spotlightId].targetLayer.toGeoJSON(),
+                currentSpotlight.spotlightShape([ev.latlng.lng, ev.latlng.lat])
+            );
+
+            // Add the highlighted features to the map as a layer
+            if (typeof currentSpotlight.highlightStyle !== "function") {
+                currentSpotlight.spotlightHighlightLayer = L.geoJSON(highlightedPoints, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, currentSpotlight.highlightStyle)
+                    }
+                }).addTo(this._map);
+            } else {
+                currentSpotlight.spotlightHighlightLayer = L.geoJSON(highlightedPoints, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, currentSpotlight.highlightStyle(feature))
+                    }
+                }).addTo(this._map);
+            }
+
+            // Add the spotlight to the map as a layer
+            currentSpotlight.spotlightLayer = L.geoJSON(currentSpotlight.spotlightShape([ev.latlng.lng, ev.latlng.lat]), {
+                style: currentSpotlight.spotlightStyle
+            }).addTo(this._map);
+
+        }
+    
     }
 
 });
 
 L.Map.addInitHook('addHandler', 'spotlight', L.SpotlightHandler);
 
-L.Map.include({
+L.Layer.Spotlight = L.Layer.extend({
 
-    _spotlightRegistry: {},
-
-});
-
-//L.Layer.Spotlight = L.Layer.extend({
-//
-//})
+})
