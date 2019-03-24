a37397b90e468aaaf95e76a06c173636f4d93e0c
iboates <iboates@gmail.com>
2019-03-24 15:17:59 +0100

    got rid of old files, remade simple example and multi spotlight example

--- js/spotlights.js
+++
@@ -1,6 +0,0 @@
-var spotlights = {
-	"circle": turf.circle([ev.latlng.lng, ev.latlng.lat], 50, {"steps": 128, "units": "meters"})
-	"rectangle": turf.envelope(turf.featureCollection([
-                    turf.circle([ev.latlng.lng, ev.latlng.lat], 50, {"steps": 128, "units": "meters"})
-                 ]))
-}
--- src/leaflet-spotlight-extension.js
+++ src/leaflet-spotlight-extension.js
@@ -87,8 +87,10 @@ L.Spotlight = L.Class.extend({
         targetLayer: 1
     },
 
-    initialize: function(id, options) {
-        this.id = id;
+    initialize: function(options) {
+        this.id = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+          );
         L.setOptions(this, options);
     },
 
